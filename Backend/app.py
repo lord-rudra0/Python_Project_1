bc588f23ff906fe6d6eec04965bc33839d0b8ff6
rudra pratap singh <prataprudra10102001@gmail.com>
2025-03-10 00:46:35 +0530

    summary generated

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,send_from_directory, send_file
+from flask import Flask,request,jsonify
 from google import genai
 from dotenv import load_dotenv
 import os
@@ -40,33 +40,37 @@ def upload():
         if page_text:  
             full_text += page_text + "\n\n"  
 
-    return full_text  # Returning extracted text  
+    return jsonify({"text": full_text})  # Returning extracted text  
 
 
 
     
+@app.route("/summary",methods=['POST'])   
+def summarize_ai():
+    data = request.get_json()  # ✅ Extracting JSON data
+    full_text = data.get("text", "")
+    client = genai.Client(api_key=GOOGLE_GEMINI_API)
+    response = client.models.generate_content(
+    model="gemini-2.0-flash", contents=f"""
+    Read all the pages of :{full_text} and Summrize the content of: {full_text}
+  
     
-# def summarize_ai(abc):
-#     client = genai.Client(api_key=GOOGLE_GEMINI_API)
-#     response = client.models.generate_content(
-#     model="gemini-2.0-flash", contents=f"""
-#     Read all the pages of :{abc} and Summrize the content of: {abc} 
-#     and 
-#     generate 5 question based on pdf in the format of 
+    
+    """
+    )
+    summary_text = response.candidates[0].content.parts[0].text if response.candidates else "No response"
+
+    return jsonify({"summary": summary_text})
+
+
+# # summrize_ai(abc)
+#   generate 5 question based on pdf in the format of 
 #      Question:
 #     option 1
 #     option2
 #     option3
 #     option4
 #     answer:
-    
-    
-#     """
-#     )
-#     print(response.text)
-
-
-# # summrize_ai(abc)
 
 # pdf_text = summarize() 
 # summarize_ai(pdf_text)
