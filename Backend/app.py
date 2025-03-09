6dc500a2f0dfc0ec4109f973ac6654bac4dee44a
rudra pratap singh <prataprudra10102001@gmail.com>
2025-03-09 20:05:59 +0530

    summarize and generating the questions

--- Backend/app.py
+++ Backend/app.py
@@ -10,24 +10,30 @@ load_dotenv()
 GOOGLE_GEMINI_API=os.getenv('GOOGLE_API')
 # print(GOPGLE_GEMINI_API)
 
-reader = PdfReader('CST303 M1 Ktunotes.in.pdf')
+
+def summarize():
+    reader = PdfReader('CST303 M1 Ktunotes.in.pdf')
 
 # printing number of pages in pdf file
-n=len(reader.pages)
-print(n)
+    n=len(reader.pages)
+    print(n)
+    full_text=" "
 
 # creating a page object
 # page = reader.pages[4]
 
 # extracting text from page
-for page in range(n):
-    page = reader.pages[page]
-    # print(page.extract_text())
-    abc=page.extract_text()
-    # print(abc)
+
+    for page in range(n):
+        current_page = reader.pages[page]
+        # print(page.extract_text())
+        page_text = current_page.extract_text()
+        full_text += page_text
+        # print(abc)
+    return full_text
     
     
-def summrize(abc):
+def summarize_ai(abc):
     client = genai.Client(api_key=GOOGLE_GEMINI_API)
     response = client.models.generate_content(
     model="gemini-2.0-flash", contents=f"Read all the pages of :{abc} and Summrize the content of: {abc} and generate 5 question based on pdf"
@@ -35,5 +41,8 @@ def summrize(abc):
     print(response.text)
 
 
-summrize(abc)
+# summrize_ai(abc)
+
+pdf_text = summarize() 
+summarize_ai(pdf_text)
 
