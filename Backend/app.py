a5d317feddca3c0bb7cf503dacff6141df56748b
Rudra Pratap Singh <130925862+lord-rudra0@users.noreply.github.com>
2025-04-07 02:58:56 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -14,7 +14,7 @@ app=Flask(__name__)
 CORS(app, resources={
     r"/upload": {"origins": ["https://rudra-ps-summer-ai.vercel.app/"]},
     r"/summary": {"origins": ["https://rudra-ps-summer-ai.vercel.app/"]},
-    r"/question": {"origins": [""]}
+    r"/question": {"origins": ["https://rudra-ps-summer-ai.vercel.app"]}
 })
 # CORS(app)
 
